21cc96c12609dddb931811179e1ff9fae4c11110
Andres Huertas <andreshcl29@gmail.com>
2018-10-31 11:02:46 -0500

    Observables

---
+++ 02-typescript/01-variables.js
@@ -0,0 +1,22 @@
+var nombre = '2312';
+var edad = 12;
+var nada = null;
+var casado = false;
+var loQueSea = {};
+loQueSea = 1;
+loQueSea = 'facil';
+loQueSea = true;
+var fechaNacimiento = new Date();
+var identificador = '1';
+identificador = 1;
+identificador = 'uno';
+var Usuario = /** @class */ (function () {
+    function Usuario() {
+    }
+    return Usuario;
+}());
+var usuario = {
+    nombre: 'Andres',
+    apellido: 'Huertas'
+};
+usuario.edad = '2';
