306ac52eff49206af915de1d623f809109638c9b
Andres Huertas <andreshcl29@gmail.com>
2018-11-05 10:58:07 -0500

    observables final

--- 02-typescript/02-observables.js
+++ 02-typescript/02-observables.js
@@ -2,11 +2,61 @@
 //import * as rxjs from 'rxjs';
 //import {Observable} from 'rxjs';
 const rxjs = require('rxjs');
-const observableUno$ = rxjs.of(1, 2, 3, 4, 5, 6, 7);
+const map = require('rxjs/operators').map;
+const disctinct = require('rxjs/operators').distinct;
+const observableUno$ = rxjs.of([1, 2, 3], 3, 'hola', 3, true, { nombre: 'andres' }, new Date(), 3, 3);
 console.log(observableUno$);
 observableUno$
+    .pipe(disctinct(), map((valor) => {
+    console.log('valor', valor);
+    return {
+        data: valor
+    };
+}))
     .subscribe((ok) => {
     console.log('en ok', ok);
-    },(error) => {
+}, (error) => {
     console.log(error);
+}, () => {
+    console.log('Completado');
 });
+const promesita = () => {
+    return new Promise((resolve, reject) => {
+        resolve(':)');
+    });
+};
+async function ejecutarCodigoSyncrono() {
+    console.log('inicio');
+    try {
+        const resultadoPromesita = await promesita();
+        console.log(resultadoPromesita);
+    }
+    catch (e) {
+        console.log('error en promesita', e);
+    }
+    console.log('fin');
+}
+ejecutarCodigoSyncrono();
+/*
+const observableDePromesa$ = rxjs.from(promesita());
+
+
+observableDePromesa$
+    .pipe(
+        map(
+            (valor)=>{
+                return{
+                    data: valor
+                }
+            }
+        )
+    )
+    .subscribe(
+        (objetoFeliz)=>{
+            console.log(objetoFeliz);
+        },
+        (error)=>{
+            console.log(error);
+        }
+    );
+*/ 
