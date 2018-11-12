249fc696fd29122a605d759a90f8b41b2109a0f1
AndreS Huertas <andreshcl29@gmail.com>
2018-11-12 11:00:39 -0500

    HTTP CLASE 11

--- 03-http/videos/src/app.controller.ts
+++ 03-http/videos/src/app.controller.ts
@@ -1,12 +1,76 @@
-import { Get, Controller } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Get, Controller, HttpCode, InternalServerErrorException, Post, Query, Param} from '@nestjs/common';
+import {AppService} from './app.service';
+import {Observable, of} from "rxjs";
 
-@Controller()
+@Controller()  //decoradores
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {
+    }
 
-  @Get()
-  root(): string {
-    return this.appService.root();
-  }
+    @Get() // http://ip:puerto
+    @HttpCode(204) // status
+    raiz(
+        @Query() todosQueryParams: any,
+        @Query('nombre') nombre: string,
+    ): string {
+        console.log(todosQueryParams);
+        return 'Hola Mundo '+ nombre;
+    }
+
+    @Get('segmentoUno/segmentoDos/:idUsuario')
+    parametroRuta(
+        @Param('idUsuario') id
+
+    ){
+        return id;
+    }
+
+    @Get('adiosMundo') // url
+    adiosMundo(): string {
+        return 'Adios Mundo';
+    }
+    @Post('adiosMundoPost') // url
+    adiosMundoPost(): string {
+        return 'Adios MundoPost';
+    }
+
+    @Get('adiosMundoPromesa') // url
+    adiosMundoPromesa(): Promise<string> {
+        const promesaAdios = (): Promise<string> => {
+            return new Promise(
+                (resolve) => {
+                    resolve('Adios Mundo');
+                }
+            )
+        };
+        return promesaAdios();
+    }
+
+
+    @Get('adiosMundoAsync') // url
+    @HttpCode(201)
+    async adiosMundoAsync() {
+        const promesaAdios = (): Promise<string> => {
+            return new Promise(
+                (resolve, reject) => {
+                    reject('Adios Mundo');
+                }
+            )
+        };
+        try {
+            const respuesta: string = await promesaAdios();
+            return respuesta;
+        } catch (e) {
+            console.error(e);
+            throw new InternalServerErrorException({mensaje: 'Error servidor'})
+        }
+
+    }
+
+    @Get('adiosMundoObservable')
+  adiosMundoObservable():Observable<string>{
+      const respuesta$ =of('Adios Mundo');
+      return respuesta$;
+    }
+
 }
