5ba61fb71f0a6694d6b6046c5c06d885bd1b5425
Andres Huertas <andrehcl29@gmail.com>
2018-11-21 10:52:28 -0500

    COOKIES, HEADERS

--- 03-http/videos/src/app.controller.ts
+++ 03-http/videos/src/app.controller.ts
@@ -1,14 +1,29 @@
-import {Get, Controller, HttpCode, InternalServerErrorException, Post, Query, Param} from '@nestjs/common';
+import {Get,
+    Controller,
+    HttpCode,
+    InternalServerErrorException,
+    Post,
+    Query,
+    Param,
+    Body,
+    Headers,
+    UnauthorizedException,
+    Res,
+    Req} from '@nestjs/common';
 import {AppService} from './app.service';
 import {Observable, of} from "rxjs";
+import {Request, Response} from 'express';
 
 @Controller()  //decoradores
+//Controller('usuario')
+//h
 export class AppController {
-    constructor(private readonly appService: AppService) {
+    constructor(private readonly _appService: AppService) { //Se puede poner mas servicion ahi mismo
+
     }
 
     @Get() // http://ip:puerto
-    @HttpCode(204) // status
+    //@HttpCode(204) // status
     raiz(
         @Query() todosQueryParams: any,
         @Query('nombre') nombre: string,
@@ -68,9 +83,46 @@ export class AppController {
     }
 
     @Get('adiosMundoObservable')
-  adiosMundoObservable():Observable<string>{
+    adiosMundoObservable():Observable<string>{
       const respuesta$ =of('Adios Mundo');
       return respuesta$;
     }
 
+    @Post('crearUsuario')
+    crearUsuario(
+        @Body() usuario: Usuario,
+        @Body('nombre') nombre:string,
+        @Headers() cabeceras,//cabeceras de peticion
+        @Headers('seguridad') codigo,
+        @Res()  res: Response,
+        @Req() req: Request,
+    ){
+        //crean usuario
+        console.log('cookies',req.cookies); // LEIDO
+        console.log('cookies seguras',req.signedCookies); // LEIDO
+        console.log(usuario);
+        console.log(cabeceras);
+
+        if (codigo === '1234') {
+            const bdd = this._appService.crearUsuario(usuario);
+
+            res.append('token','5678');
+            res.cookie("app","web"); //Insegura  //enviar cookies inseguras
+            res.cookie("segura","essecreto",{
+                signed: true,
+            });
+            res.json(bdd);
+        }else{
+            throw new UnauthorizedException({
+                mensaje: 'Error de autorizacion',
+                error: 401
+            })
+        }
+
+    }
+
 }
+
+   export interface Usuario {
+    nombre: string;
+    }
--- 03-http/videos/src/app.service.ts
+++ 03-http/videos/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import {Usuario} from "./app.controller";
 
 @Injectable()
 export class AppService {
-  root(): string {
-    return 'Hello World!';
+  bdd: Usuario[]= [];  //Archivo .JSON
+
+  crearUsuario(usuario: Usuario){
+    this.bdd.push(usuario);
+    return this.bdd;
+
   }
+
 }
