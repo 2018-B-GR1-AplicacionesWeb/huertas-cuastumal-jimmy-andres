ec013e5401f388ffe5202451681e188c5218b7ba
Andres Huertas <andreshcl29@gmail.com>
2018-12-12 12:08:04 -0500

    ejs

--- 03-http/videos/src/app.module.ts
+++ 03-http/videos/src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {NoticiaService} from "./noticia.service";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [],  // MODULOS
+    controllers: [AppController],  // Controllers
+    providers: [
+        AppService,
+        NoticiaService
+    ], // Servicios
 })
-export class AppModule {}
+export class AppModule {
+}
--- 03-http/videos/src/main.ts
+++ 03-http/videos/src/main.ts
@@ -1,18 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-//const http_server = require('http-server) //js
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
+// const http_server = require('http-server'); // JS
 import * as cookieParser from 'cookie-parser';
-import * as httpserver from 'http-server';
-console.log(httpserver);
+import * as ejs from 'ejs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser('me gustan los tacos', //secreto
-      {
-        //opciones
-      })
-  );
-  await app.listen(2000);
+    const app = await NestFactory.create(AppModule);
+    app.use(cookieParser(
+        'me gustan los tacos', // secreto
+        {  // opciones
+
+        }
+    ));
+    app.set('view engine', 'ejs');
+
+    await app.listen(3000);
 }
+
 bootstrap();
