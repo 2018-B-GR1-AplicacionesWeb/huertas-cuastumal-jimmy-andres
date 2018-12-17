1ee11ba3311cbad0ba40cf21f2e969c883e02e2a
Andres Huertas <andreshcl29@gmail.com>
2018-12-17 10:51:45 -0500

    Mensaje actualizar y crear

--- http/videos/src/noticia.service.ts
+++ http/videos/src/noticia.service.ts
@@ -59,4 +59,14 @@ export class NoticiaService {
         return this.arreglo[indiceNoticia]
     }
 
+    buscarPorId(idNoticia: number): Noticia {
+        const indiceNoticia = this.arreglo
+            .findIndex(
+                (noticia) => {
+                    return noticia.id === idNoticia
+                }
+            );
+        return this.arreglo[indiceNoticia];
+    }
+
 }
