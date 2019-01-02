39f917c17cf69e0659635d72a0133aaec5587e51
Andres Huertas <andreshcl29@gmail.com>
2019-01-02 10:43:14 -0500

    bdd

--- http/videos/src/app.module.ts
+++ http/videos/src/app.module.ts
@@ -2,9 +2,22 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {NoticiaService} from "./noticia.service";
+import {TypeOrmModule} from '@nestjs/typeorm'
+
 
 @Module({
-    imports: [],  // MODULOS
+    imports: [
+        TypeOrmModule.forRoot(
+            {
+                type:'mysql',
+                host:'localhost',
+                port: 32773,
+                database:'noticias',
+                username: 'andreshcl',
+                password: '12345'
+            }
+        )
+    ],  // MODULOS
     controllers: [AppController],  // Controllers
     providers: [
         AppService,
